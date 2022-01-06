8ae4a74b05273f4a46e1311948b4193fe8766bb1
dinghaoxiang <michael19950415@outlook.com>
2022-01-06 14:07:39 +0800

    update

--- OutputDebug/osx-comile.bat
+++ OutputDebug/osx-comile.bat
@@ -3,6 +3,6 @@ set root=%cd%
 cd ..\Bilibili.Manga.Avalonia
 set Buildx64=%root%\Publish\osx-x64
 set BuildArm=%root%\Publish\osx-arm64
-dotnet publish Bilibili.Manga.Avalonia.csproj -o %Buildx64% --runtime osx.11.0-x64 --self-contained true /p:Configuration=Release
+dotnet publish Bilibili.Manga.Avalonia.csproj -o %Buildx64% --runtime osx-x64 --self-contained true /p:Configuration=Release
 dotnet publish Bilibili.Manga.Avalonia.csproj -o %BuildArm% --runtime osx.11.0-arm64 --self-contained true /p:Configuration=Release
 pause
