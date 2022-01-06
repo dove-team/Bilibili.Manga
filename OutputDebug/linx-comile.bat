1cf29e9737fa417149b5a763656455c44e3da0ba
dinghaoxiang <michael19950415@outlook.com>
2022-01-06 16:27:11 +0800

    Update linx-comile.bat

--- OutputDebug/linx-comile.bat
+++ OutputDebug/linx-comile.bat
@@ -4,7 +4,8 @@ dotnet tool install --global dotnet-deb
 dotnet tool install --global dotnet-zip
 cd ..\Bilibili.Manga.Avalonia
 set Buildx64=%root%\Publish\linux-x64
-set BuildArm=%root%\Publish\linux-arm64
+set BuildArm=%root%\Publish\linux-arm
+set BuildArm64=%root%\Publish\linux-arm64
 ::linux-x64
 dotnet restore -r linux-x64
 dotnet deb install
@@ -13,4 +14,8 @@ dotnet msbuild Bilibili.Manga.Avalonia.csproj -property:OutDir=%Buildx64% /t:Cre
 dotnet restore -r linux-arm
 dotnet deb install
 dotnet msbuild Bilibili.Manga.Avalonia.csproj -property:OutDir=%BuildArm% /t:CreateDeb /p:TargetFramework=net6.0 /p:RuntimeIdentifier=linux-arm /p:Configuration=Release
+::linux-arm64
+dotnet restore -r linux-arm64
+dotnet deb install
+dotnet msbuild Bilibili.Manga.Avalonia.csproj -property:OutDir=%BuildArm64% /t:CreateDeb /p:TargetFramework=net6.0 /p:RuntimeIdentifier=linux-arm64 /p:Configuration=Release
 pause
